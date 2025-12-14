feafeec1b1779684fbcb67b0e56b933c11aa37a5
Menna-Nabil <mennanabil550@gmail.com>
2025-12-15 01:43:05 +0200

    Update cpp

--- MennaNabil.cpp
+++ MennaNabil.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
-
 using namespace std;
 
 int main() {
     cout << "Hello MennaNabil" << endl;
-
-    
-    for (int i = 1;; ++i) {
-        cout << "Container is running..." << i << endl;
-        this_thread::sleep_for(chrono::seconds(5)); 
-    }
 
     return 0; 
 }
